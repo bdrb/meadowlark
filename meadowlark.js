406b06dd7da5ffc857ab5473646f2c563a21d571
bdrb <bdrb@ukr.net>
2016-08-28 21:24:59 +0300

    fortunes in module

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -13,21 +14,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
    res.render('home');
-});
+    });
 
 /////////////////////////
 
 
 app.get('/about', function(req, res){
-    var fortunes = [
-        "Conquer your fears or they will conquer you.",
-        "Rivers need springs.",
-        "Do not fear what you don't know.",
-        "You will have a pleasant surprise.",
-        "Whenever possible, keep it simple.",
-    ];
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 app.use(function(req, res){
