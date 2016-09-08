9a28279d64c0e2bf538e74126b3322fb72ca6ad0
bdrb <bdrb@ukr.net>
2016-09-08 21:54:05 +0300

    qa 01

--- meadowlark.js
+++ meadowlark.js
@@ -13,8 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next){
-    res.locals.showTests = 
-        app.get('env') !== 'production' && req.query.test = 1;
+    res.locals.showTests = (app.get('env') !== 'production' && req.query.test === '1');
     next();
 });
 
@@ -24,10 +23,20 @@ app.get('/', function(req, res){
 
 /////////////////////////
 
-
 app.get('/about', function(req, res){
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('/tours/hood-river'});
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('/tours/request-group-rate'});
+	});
 
 app.use(function(req, res){
     res.status('404');
